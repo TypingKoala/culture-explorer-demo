5db95811fb27cce8b615b9b0c8e38ca90d749560
Johnny Bui <typngeek@aim.com>
2020-01-07 11:54:48 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/Artwork.tsx
+++ src/Artwork.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import { Image, Text, Stack, mergeStyles} from 'office-ui-fabric-react';
+
+import {Image, Text, Stack, ImageFit, mergeStyles} from 'office-ui-fabric-react';
+import GalleryItem from './GalleryItem';
 
 const spacerB = mergeStyles({
     paddingBottom: 10,
   });
 
-class Artwork extends React.Component{
-  render(){
-      return(
-            <Stack
-            horizontalAlign="center"
-            verticalAlign="start"
-            className={spacerB}
-            >
-                <Image
-                    src='//q42imageserver.appspot.com/thumbnail/aa08df9c-0af9-4195-b31b-f578fbe0a4c9?cx=893&cy=702&cw=954&ch=954&w=200&h=200'
-                    className={spacerB}
-                />
-                <Text variant="large">The Night Watch, Rembrandt van Rijn, 1642</Text>
-                <Text variant="small">oil on canvas</Text>
+const artworkImageClass = mergeStyles({
+  width: "inherit"
+});
+
+type ArtworkProps = {
+    item: GalleryItem
+}
+class Artwork extends React.Component < ArtworkProps > {
+    render() {
+        return (
+            <Stack horizontalAlign="center" verticalAlign="start" className={spacerB}>
+                <Image src={this.props.item.url} imageFit={ImageFit.centerContain} width={400} height={200} className={spacerB}/>
+                <Text variant="large">{this.props.item.title}</Text>
+                <Text variant="small">{this.props.item.desc}</Text>
             </Stack>
-      )
-  }
+        )
+    }
 };
 
 export default Artwork;
