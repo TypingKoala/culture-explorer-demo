c2cb43bfd6a524f8e5ec523431f70f9ef68ec28d
Zhenbang Chen (Populus Group) <v-zhenc@microsoft.com>
2020-01-07 10:34:17 -0500

    Styles for options

--- src/Options.tsx
+++ src/Options.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Stack, FontWeights, Dropdown, IDropdownOption, IDropdownStyles} from 'office-ui-fabric-react';
+import { mergeStyles } from '@uifabric/merge-styles';
 
 const boldStyle = {
   root: { fontWeight: FontWeights.semibold }
 }
 
+const dropdown = mergeStyles({
+    marginBottom : 10
+})
+
 interface IState {
 }
 
 interface IProps {
+
 }
 
 const dropdownStyles: Partial<IDropdownStyles> = {
@@ -35,31 +41,32 @@ class Options extends React.Component<IProps, IState> {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange() {
+  handleChange(event : React.FormEvent<HTMLDivElement>, option?: IDropdownOption,) {
+    console.log(option)
   }
 
   render() {
     return(
-        <Stack>
-          <Stack.Item align="center">
-            <Dropdown
-              placeholder="Select Culture"
-              label="Culture"
-              options={cultureOptions}
-              styles={dropdownStyles}
-              style={{"marginBottom" : "10px"}}
-              onChange={this.handleChange}
-            />
-            <Dropdown
-              placeholder="Select Medium"
-              label="Medium"
-              options={mediumOptions}
-              styles={dropdownStyles}
-              style={{"marginBottom" : "10px"}}
-              onChange={this.handleChange}
-            />
-          </Stack.Item>
-        </Stack>
+      <Stack>
+        <Stack.Item align="center">
+          <Dropdown
+            placeholder="Select Culture"
+            label="Culture"
+            options={cultureOptions}
+            styles={dropdownStyles}
+            className={dropdown}
+            onChange={this.handleChange}
+          />
+          <Dropdown
+            placeholder="Select Medium"
+            label="Medium"
+            options={mediumOptions}
+            styles={dropdownStyles}
+            className={dropdown}
+            onChange={this.handleChange}
+          />
+        </Stack.Item>
+      </Stack>
     );
     
   }
