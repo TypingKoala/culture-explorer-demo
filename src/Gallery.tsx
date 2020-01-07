8fc0fdf99026683ddfb848979f4d9b26403c1435
Johnny Bui <typngeek@aim.com>
2020-01-07 16:44:32 -0500

    put gallery into one file

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
 import { FocusZone, List, IRectangle, ITheme, getTheme, mergeStyleSets, mergeStyles } from 'office-ui-fabric-react';
+import {
+  DocumentCard,
+  DocumentCardPreview,
+  IDocumentCardPreviewProps
+} from 'office-ui-fabric-react/lib/DocumentCard';
+import { ImageFit } from 'office-ui-fabric-react/lib/Image';
 import GalleryItem from './GalleryItem';
-import { GalleryCard } from './Gallery2';
+
+type CardProps = {
+  item: GalleryItem
+  setSelected: any
+}
+
+export class GalleryCard extends React.PureComponent<CardProps> {
+
+  public render(): JSX.Element {
+    const previewProps: IDocumentCardPreviewProps = {
+      previewImages: [
+        {
+          name: this.props.item.title,
+          linkProps: {
+            href: '#'
+          },
+          previewImageSrc: this.props.item.url,
+          imageFit: ImageFit.cover,
+          width: 200,
+          height: 150
+        }
+      ]
+    };
+
+    return (
+      <DocumentCard
+        onClick={()=>this.props.setSelected(this.props.item)}
+      >
+        <DocumentCardPreview {...previewProps} />
+        {/* <DocumentCardTitle
+          title="The Nightwatch"
+          shouldTruncate={true}
+        /> */}
+      </DocumentCard>
+    );
+  }
+}
 
 //from site- scrollable list
 
