05e9ad95eeeee9bbb2fa3ebbb5baeec53d4befdc
Johnny Bui <typngeek@aim.com>
2020-01-07 09:18:21 -0500

    Replace divs with stacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import {Stack, Text, FontWeights} from 'office-ui-fabric-react';
-
-const boldStyle = {
-    root: {
-        fontWeight: FontWeights.semibold
-    }
-};
+import {Stack, Text} from 'office-ui-fabric-react';
 
 export const App : React.FunctionComponent = () => {
     return (
         <Stack>
-            <div className="ms-Grid" dir="ltr">
-                <div className="ms-Grid-row">
-                    {/*header*/}
-                </div>
-                <div className="ms-Grid-row">
-                    <div className="ms-Grid-col ms-sm4">
-                      <Text>Column A</Text>
-                    </div>
-                    <div className="ms-Grid-col ms-sm4">
-                      <Text>Column B</Text>
-                    </div>
-                    <div className="ms-Grid-col ms-sm4">
-                      <Text>Coumn C</Text>
-                    </div>
-                </div>
-            </div>
+            <Stack>
+                {/*header*/}
+            </Stack>
+            <Stack horizontal>
+                <Stack grow={1}>
+                    <Text>Column A</Text>
+                </Stack>
+                <Stack grow={1}>
+                    <Text>Column B</Text>
+                </Stack>
+                <Stack grow={1}>
+                    <Text>Column C</Text>
+                </Stack>
+            </Stack>
         </Stack>
 
     );
